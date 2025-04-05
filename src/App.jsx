12f14209ec2cd7de8faf6bfe5b9b09a14795b5d8
avajamdade <ava.jamdade@gmail.com>
2025-04-05 14:30:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import Food from './Food'
+import Card from './Card'
+
+function App() {
+  
+   return(
+    <>
+      <Card></Card>
+    </>
+   )  
+};
+
+export default App
---
+++ src/Food.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Food(){
+
+    const food1 = "Mango";
+    const food2 = "Orange";
+
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1.toLowerCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    )
+
+}
+
+export default Food
